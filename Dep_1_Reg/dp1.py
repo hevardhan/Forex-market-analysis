2f771aeb6fef59d26e8502fa88942aa62bc6f780
hevardhan <hevardhan2004@gmail.com>
2023-10-25 15:32:26 +0530

    heva

--- Dep_1_Reg/dp1.py
+++ Dep_1_Reg/dp1.py
@@ -1,56 +1,121 @@
-import pickle
 import MetaTrader5 as mt5
 import pandas as pd
-with open('Dep_1_Reg\lin_reg.pkl', 'rb') as file:
-    model = pickle.load(file)
+import pickle
+import numpy as np
+import tkinter as t
+from tkinter import ttk
+from tkinter import messagebox
+mt5.initialize(path=r"C:\Program Files\MetaTrader 5 IC Markets (SC)\terminal64.exe")
+def start_trading():
+    SYMBOL = n.get()
+    VOLUME = float(vol_entry.get())
+    DEVIATION = int(dev_entry.get())
+    def market_order(symbol, volume, order_type, **kwargs):
+        tick = mt5.symbol_info_tick(symbol)
+        point = mt5.symbol_info(symbol).point
+        order_dict = {'buy': 0, 'sell': 1}
+        price_dict = {'buy': tick.ask, 'sell': tick.bid}
+        # if order_type == 'buy':
+        #     sl = price_dict[order_type] - 50 * point
+        #     tp = price_dict[order_type] + 100 * point
+        # elif order_type == 'sell':
+        #     sl = price_dict[order_type] + 50 * point
+        #     tp = price_dict[order_type] - 100 * point
+        request = {
+            "action": mt5.TRADE_ACTION_DEAL,
+            "symbol": symbol,
+            "volume": volume,
+            "type": order_dict[order_type],
+            "price": price_dict[order_type],
+            "deviation": DEVIATION,
+            # "sl" : sl,
+            # "tp" : tp,
+            "magic": 100,
+            "comment": "python market order",
+            "type_time": mt5.ORDER_TIME_GTC,
+            "type_filling": mt5.ORDER_FILLING_IOC,
+        }
 
-mt5.initialize()
-#-------------------------------------------------------------------------------------------------------------------------------
+        order_result = mt5.order_send(request)
+        print(order_result)
 
-#INPUTS
-SYMBOL       = input("Enter the Symbol")
-TIMEFRAME    = mt5.TIMEFRAME_H4
-VOLUME       = 1.0
-DEVIATION    = 10
-point        = mt5.symbol_info(SYMBOL).point
-MAX_DIST_SL  = 50  * point
-TRAIL_AMOUNT = 40  * point 
-DEFAULT_SL   = 40  * point
-TP           = 100 * point
+        return order_result
 
-#--------------------------------------------------------------------------------------------------------------------------------
 
-def market_order(symbol, volume, order_type, **kwargs):
-    tick = mt5.symbol_info_tick(symbol)
-    point = mt5.symbol_info(symbol).point
-    order_dict = {'buy': 0, 'sell': 1}
-    price_dict = {'buy': tick.ask, 'sell': tick.bid}
-    if order_type == 'buy':
-        sl = price_dict[order_type] - 50 * point
-        tp = price_dict[order_type] + 100 * point
-    elif order_type == 'sell':
-        sl = price_dict[order_type] + 50 * point
-        tp = price_dict[order_type] - 100 * point
-    request = {
-        "action": mt5.TRADE_ACTION_DEAL,
-        "symbol": symbol,
-        "volume": volume,
-        "type": order_dict[order_type],
-        "price": price_dict[order_type],
-        "deviation": DEVIATION,
-        "sl" : sl,
-        "tp" : tp,
-        "magic": 100,
-        "comment": "python market order",
-        "type_time": mt5.ORDER_TIME_GTC,
-        "type_filling": mt5.ORDER_FILLING_IOC,
-    }
+    TIMEFRAME = mt5.TIMEFRAME_H4
+    rates = mt5.copy_rates_from_pos(SYMBOL,TIMEFRAME,0,20)
+    df = pd.DataFrame(rates)
+    df['SMA10'] = df['close'].rolling(10).mean()
+    df['SMA20'] = df['close'].rolling(20).mean()
+    def signal(raw):
+        
+        if raw['SMA10'] > raw['SMA20']:
+            return 1
+        elif raw['SMA10']  < raw['SMA20']:
+            return 0
+        else :
+            return np.NaN
 
-    order_result = mt5.order_send(request)
-    print(order_result)
+    df['Signal'] = df.apply(signal,axis=1)
+    # df['time'] = pd.to_datetime(df['time'],unit='s')
+    # print(type(df['time'][0]))
+    # df.rename(columns={'time':'timestamp_unix'},inplace=True)
+    df['timestamp_unix'] = df['time'].astype(np.int64) / 10**9 
+    df.dropna(inplace=True)
+    if mo_ch.get() == 'Linear Regression':
+        model = pickle.load(open(r"D:\SY - Class\Forex-market-analysis\Dep_1_Reg\lin_reg.pkl","rb"))
+    else : 
+        model = pickle.load(open(r"rfc.pkl","rb"))
+    y = model.predict(df[['timestamp_unix','open','high','low','close','SMA10','Signal','SMA20']])
+    # signal = [1 if p[0] >= 0.5 else 0 for p in y]
+    # print(y)
+    cat = [1 if p[0] >= 0.5 else 0 for p in y]
+    # print(signal)
+    signal = df['Signal'].to_list()
+    print(cat)
+    # # print(signal)
+    if cat[0] == 1:
+        if signal[0] == 1:
+            market_order(SYMBOL,VOLUME,"buy")
+            messagebox.showinfo("","Buy Order Placed Successfully")
+        elif signal[0] == 0:
+            market_order(SYMBOL,VOLUME,'sell')
+            messagebox.showinfo("","Sell Order Placed Successfully")
 
-    return order_result
+main = t.Tk()
+main.state('zoomed')
+main.resizable(0,0)
 
-rates = mt5.copy_rates_from_pos(SYMBOL,TIMEFRAME,0,1)
-rates_df = pd.DataFrame(rates)
-print(rates_df)
+label = t.Label(main,text='Algorithmic Trading',font=('Poppins',30))
+txt_3 =     t.Label(main,text="Choose the Model : ",font=('Poppins Light',15))
+mo_ch = t.StringVar()
+mo_choice = ttk.Combobox(main,textvariable=mo_ch)
+mo_choice['values'] = ('Linear Regression','Random Forest Classifier')
+n = t.StringVar() 
+choice = ttk.Combobox(main,textvariable=n)
+choice['values'] = ('EURUSD')
+
+txt_1 =     t.Label(main,text="Choose the Symbol : ",font=('Poppins Light',15))
+dev_label = t.Label(main,text='Enter the Deviation : ',font=('Poppins Light',15))
+dev_entry = t.Entry(main,font=('Poppins Light',12))
+
+vol_label = t.Label(main,text="Enter the Volume    : ",font=('Poppins Light',15))
+vol_entry = t.Entry(main,font=('Poppins Light',12))
+
+start_trade_btn = t.Button(main,text="Predict and Place Order",font=('Poppins Light',12),command=start_trading)
+label.place(x=200,y=100)
+txt_1.place(x=200,y=200)
+choice.place(x=420,y=210)
+
+txt_3.place(x=200,y=550)
+mo_choice.place(x=420,y=560)
+
+dev_label.place(x=200,y=300)
+dev_entry.place(x=420,y=305)
+
+vol_label.place(x=200,y=400)
+vol_entry.place(x=420,y=405)
+
+start_trade_btn.place(x=200,y=650)
+main.mainloop()
+
--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 import datetime
+import pickle
+import numpy as np
+import pytz
 st.title("Forex-Market-Analysis")
-choice = st.sidebar.selectbox("Select the Symbol",("EURUSD","AUDUSD","USDCHF"))
-tf_choice = st.sidebar.selectbox("Select the Timeframe",("1 Min","5 Min","15 Min","30 Min","1 Hr","4 Hr","12 hr","Daily","Weekly","Monthly"))
-from_date = st.sidebar.date_input("From :",value=None,format="DD.MM.YYYY")
-to_date = st.sidebar.date_input("To :",value=None,format="DD.MM.YYYY")
+choice = st.sidebar.selectbox("Select the Symbol",("EURUSD"," "))
 
-if st.sidebar.button("Done"):
-    print("Done")
-    graph_choice = st.selectbox("Choose the Type of graph",("Candle Stick","Box plot","Scatter Plot","Histogram","Distribution Plot","Pie Chart","Line plot"))
-    if choice == "EURUSD":
-        # tf_choice = st.selectbox("Select the Timeframe",("1 Min","5 Min","15 Min","30 Min","1 Hr","4 Hr","12 hr","Daily","Weekly","Monthly"))
-        if tf_choice == "1 Min":
-            df = pd.read_csv(r"C:\Users\sitadmin\Desktop\Hevardhan\Forex-market-analysis\datasets\datasets\eurusd\eurusd_m1.csv")
-            del df['Unnamed: 0']
-            slider = st.number_input("Enter the Number of Rows to Display",value=10)       
-            st.dataframe(df.head(slider))
-            ddf = df.head(slider)
-            if st.button("Show graph"):
+
+type_choice = st.sidebar.selectbox("Choose the Operation",("Visualization","Prediction"))
+if choice == "EURUSD":
+    if type_choice == 'Visualization':
+        graph_choice = st.selectbox("Choose the Type of graph",("Candle Stick","Box plot","Histogram","Line plot"))
+        # tf_choice = st.selectbox("Select the Timeframe",("5 Min","15 Min","30 Min","1 Hr","4 Hr","12 hr","Daily","Weekly","Monthly"))
+        # if tf_choice == "4 Hr":
+        df = pd.read_csv(r"C:\Users\sarav\Documents\EDA\datasets\eurusd\eurusd_h4.csv")
+        del df['Unnamed: 0']
+        slider = st.number_input("Enter the Number of Rows to Display",value=10)       
+        st.dataframe(df.head(slider))
+        ddf = df.head(slider)
+        if st.button("Show graph"):
+            if graph_choice == 'Candle Stick':
                 fig = go.Figure(data=[go.Candlestick(x=ddf['time'],
                     open=ddf['open'],
                     high=ddf['high'],
@@ -27,5 +30,115 @@ if st.sidebar.button("Done"):
                     close=ddf['close'])])
 
                 st.plotly_chart(fig)
+            elif graph_choice == 'Box plot':
+                fig = px.box(ddf, y=["open", "high", "low", "close"], boxmode="overlay")  # Replace "Category" and "Value" with your actual column name
+                fig.update_layout(
+                    title="Box Plot of Open, High, Low, and Close Values",
+                    yaxis_title="Price",
+                )
+                # Show the plot
+                st.plotly_chart(fig)
+            elif graph_choice == 'Histogram':
+                fig = px.histogram(ddf, x=["open", "high", "low", "close"])
+
+                # Customize the plot
+                fig.update_layout(
+                    title="Histogram of Open, High, Low, and Close Values",
+                    xaxis_title="Price",
+                    yaxis_title="Count",
+                )
+                st.plotly_chart(fig)
+            elif graph_choice == 'Line plot':
+                ddf['SMA10'] = ddf['close'].rolling(10).mean()
+                ddf['SMA20'] = ddf['close'].rolling(20).mean()
+                fig = px.line(ddf, x="time", y=["close", "SMA10", "SMA20"])
+
+                # Customize the plot (optional)
+                fig.update_layout(
+                    xaxis_title="Time",
+                    yaxis_title="Value",
+                    title="Line Plot of Close, SMA10, and SMA20",
+                )
+                st.plotly_chart(fig)
+                
+
+    else :
+        model_choose = st.selectbox("Select the Model",("Random Forest Classification","Linear Regression")) 
+        open_   = st.number_input("Enter Open Price")
+        high   = st.number_input("Enter High Price")
+        low    = st.number_input("Enter Low Price")
+        close  = st.number_input("Enter Close Price")
+        SMA10  = st.number_input("Enter SMA 10 Value")
+        SMA20  = st.number_input("Enter SMA 20 Value")
+        # Create a DataFrame
+        data = {
+            "open": [open_],
+            "high": [high],
+            "low": [low],
+            "close": [close],
+            "SMA10": [SMA10],
+            "SMA20": [SMA20],
+        }
+        df = pd.DataFrame(data)
+        timezone = pytz.timezone("Etc/UTC")
+        df['time'] = datetime.datetime.now()
+        df['timestamp_unix'] = (df['time'].astype(np.int64) / 10**9).astype(int)
+        def signal(raw):
             
+            if raw['SMA10'] > raw['SMA20']:
+                return 1
+            elif raw['SMA10']  < raw['SMA20']:
+                return 0
+            else :
+                return None
+
+        df['Signal'] = df.apply(signal,axis=1)
+        signal = df['Signal'].to_list()
+        
+        if st.button("show"):
+            if model_choose == 'Linear Regression':
+                # Load the pickled model
+                with open(r"Dep_1_Reg\lin_reg.pkl", "rb") as model_file:
+                    model = pickle.load(model_file)
+                if signal[0] != None:
+                    y = model.predict(df[['timestamp_unix','open','high','low','close','SMA10','Signal','SMA20']])
+                    # signal = [1 if p[0] >= 0.5 else 0 for p in y]
+                    # print(y)
+                    cat = [1 if p[0] >= 0.5 else 0 for p in y]
+                    # print(signal)
+                    # print(cat)
+                    # # print(signal)
+                    if cat[0] == 1:
+                        if signal[0] == 1:
+                            st.info(f"Profitable Signal : You can BUY the stock of {choice}.")
+                        elif signal[0] == 0:
+                            st.info(f"Profitable Signal : You can SELL the stock of {choice}.")
+                    elif cat[0] == 0:
+                        st.warning(f"Non Profitable Signal : Dont BUY or SELL any stock of {choice}")
+                else :
+                    st.warning("Incorrect Input")
+            elif model_choose == 'Random Forest Classification':
+                # Load the pickled model
+                with open(r"rfc.pkl", "rb") as model_file:
+                    model = pickle.load(model_file)
+                if signal[0] != None:
+                    y = model.predict(df[['timestamp_unix','open','high','low','close','SMA10','Signal','SMA20']])
+                    # signal = [1 if p[0] >= 0.5 else 0 for p in y]
+                    # print(y)
+                    # cat = [1 if p[0] >= 0.5 else 0 for p in y]
+                    # print(signal)
+                    # print(cat)
+                    # # print(signal)
+                    if y[0] == 1:
+                        if signal[0] == 1:
+                            st.info(f"Profitable Signal : You can BUY the stock of {choice}.")
+                        elif signal[0] == 0:
+                            st.info(f"Profitable Signal : You can SELL the stock of {choice}.")
+                    elif y[0] == 0:
+                        st.warning(f"Non Profitable Signal : Dont BUY or SELL any stock of {choice}")
+                else :
+                    st.warning("Incorrect Input")
+                
+
+
 
