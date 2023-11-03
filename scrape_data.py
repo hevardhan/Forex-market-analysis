d13103730acebb93366c2b6b1e4904dad5717d43
hevardhan <hevardhan2004@gmail.com>
2023-11-03 08:21:30 +0530

    Final commit

--- scrape_data.py
+++ scrape_data.py
@@ -8,11 +8,11 @@ import pytz
 mt5.initialize(path=r"C:\Program Files\MetaTrader 5 IC Markets (SC)\terminal64.exe")
 timezone = pytz.timezone("Etc/UTC")
 
-utc_from = datetime.datetime(2012, 1, 1, tzinfo=timezone)
-utc_to = datetime.datetime(2021, 12, 31, tzinfo=timezone)
-rates = mt5.copy_rates_range("USDCHF", mt5.TIMEFRAME_H4, utc_from, utc_to)
+utc_from = datetime.datetime(2022, 1, 1, tzinfo=timezone)
+utc_to = datetime.datetime(2022, 12, 31, tzinfo=timezone)
+rates = mt5.copy_rates_range("EURUSD", mt5.TIMEFRAME_H4, utc_from, utc_to)
 
 df = pd.DataFrame(rates)
 df['time'] = pd.to_datetime(df['time'],unit='s')
-df.to_csv(r"C:\Users\sarav\Documents\EDA\datasets\usdchf\usdchf_h4.csv")
+df.to_csv(r"C:\Users\sarav\Documents\EDA\datasets\eurusd\eurusd_test.csv")
 print(df)
